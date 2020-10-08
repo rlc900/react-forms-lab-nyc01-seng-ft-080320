c5a260f3c2d9d57ecdf37a5f35789c93cabb6c42
Rianna Cleary <rianna.cleary@flatironschool.com>
2020-10-08 22:28:12 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,30 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {input: ''};
+  }
+
+  handleOnChange = (evt) => {
+    this.setState({
+      input: evt.target.value
+    })
   }
 
   render() {
+    let {input} = this.state
+    let {maxChars} = this.props
+    let remainingChars = maxChars - input.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+        type="text"
+        name="message"
+        id="message"
+        value={input}
+        onChange={evt => this.handleOnChange(evt)}
+        />
+        <h1>Remaining Chars: {remainingChars}</h1>
       </div>
     );
   }
